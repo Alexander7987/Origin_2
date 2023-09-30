2defd578e23a447bb1f5e12c1c36a3ef2716e1c5
Alexander Zhuravlev <a-798@mail.ru>
2023-09-30 16:28:17 +0300

    change Lesson_5 Task_2

--- Lesson_5/ConsoleApplication1_Task_2/ConsoleApplication1_Task_2.cpp
+++ Lesson_5/ConsoleApplication1_Task_2/ConsoleApplication1_Task_2.cpp
@@ -8,6 +8,131 @@ private:
     T** test;
     int row, column;
 public:
+
+
+    class second_index
+    {
+    private:
+        T* test;
+        int column;
+    public:
+        second_index(T* test, int column) : test(test), column(column)
+        {
+        }
+        T& operator [](const int index) //неконстантная версия перегрузки оператора индексирования для класса-обертки двумерного дин. массива
+        {
+            try
+            {
+                if (index < column)
+                    return test[index];
+                else
+                    throw std::logic_error("Wrong second index");
+            }
+            catch (std::logic_error& logic)
+            {
+                std::cout << logic.what();
+            }
+        }
+    };
+
+
+    table(int row, int column)
+    {
+        this->row = row;
+        this->column = column;
+        test = new T * [this->row];
+        for (int i = 0; i < this->row; i++)
+        {
+            test[i] = new T[this->column];
+        }
+    }
+
+    table(const table&) = delete; //запрещаем конструктор копирования
+
+    table& operator = (const table&) = delete; //запрещаем конструктор копирования
+
+    ~table()
+    {
+        for (int i = 0; i < this->row; i++)
+        {
+            delete[]test[i];
+        }
+        delete[] test;
+    }
+
+    void Size()
+    {
+        std::cout << std::endl << "Размер таблицы: " << this->row << " x " << this->column << std::endl;
+    }
+
+
+
+    second_index operator [](const int index) //неконстантная версия перегрузки оператора индексирования для класса-обертки двумерного дин. массива
+    {
+        try
+        {
+            if (index < row)
+                return second_index{ test[index], column };
+            else
+                throw std::logic_error("Wrong index");
+        }
+        catch (std::logic_error& logic)
+        {
+            std::cout << logic.what();
+        }
+    }
+
+    second_index operator [](const int index) const //константная версия перегрузки оператора индексирования для класса-обертки двумерного дин. массива
+    {
+        try
+        {
+            if (index < row)
+                return second_index{ test[index], column };
+            else
+                throw std::logic_error("Wrong index");
+        }
+        catch (std::logic_error& logic)
+        {
+            std::cout << logic.what();
+        }
+    }  
+};
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    table<int> test(2, 3);
+    test[0][0] = 4;  //в классе перегружаем для объектов класса test оператор"[]" для того, чтобы вернуть от туда указатель на строку двумерного массива, которая, в свою очередь,  
+    // является одномерным динамическим массивом, в котором располагаются уже непосредственно сами числа. В перегрузке оператора [] в классе test, 
+    // где возвращаем (через return) указатель на выбранную строку (если она не > row), "оборачиваем" её в объект класса second_index, который создан для проверки второго оператора 
+    // индексирования. Т.о. мы создаем объект класса second_index, в котором через конструктор присваиваем поля - указателя на полученный одномерный массив и кол-во "столбцов".
+    // Затем в этом же классе second_index перегружаем оператор [] для получения адреса уже непосредственно ячейки в нашем одномерном массиве (который был получен через
+    // указатель test). Важно помнить, что возвращаем именно адрес!
+    std::cout << test[0][0];
+    std::cout << std::endl;
+    test.Size();  
+    return 0;
+}
+
+
+
+
+
+
+/*#include <iostream>
+#include <Windows.h>
+
+template<typename T>
+class table
+{
+private:
+    T** test;
+    int row, column;
+public:
     table(int row, int column)
     {
         this->row = row;
@@ -18,12 +143,18 @@ public:
             test[i] = new T[this->column];
         }
     }
+
+    table(const table&) = delete; //запрещаем конструктор копирования
+
+    table& operator = (const table&) = delete; //запрещаем конструктор копирования
+
     ~table()
     {
         for (int i = 0; i < this->row; i++)
         {
             delete []test[i];
         }
+        delete [] test;
         delete test;
     }
 
@@ -46,6 +177,7 @@ public:
            std::cout << logic.what();
        }
     }
+
     T* operator [](const int index) const
     {
         try
@@ -76,4 +208,4 @@ int main()
     std::cout << std::endl;
     test.Size();
     return 0;
-}
+} */
