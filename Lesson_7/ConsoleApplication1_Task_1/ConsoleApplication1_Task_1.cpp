b636a58c578e03b713448f1dd9a25d4d9c9658e3
Alexander Zhuravlev <a-798@mail.ru>
2023-10-05 17:54:51 +0300

    change Lesson_5 (Task_2), add Lesson_7

---
+++ Lesson_7/ConsoleApplication1_Task_1/ConsoleApplication1_Task_1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void foo(std::vector<int>& myVector)
+{
+    sort(myVector.begin(), myVector.end());
+    auto result = unique(myVector.begin(), myVector.end());
+    myVector.erase(result, myVector.end());
+}
+
+int main()
+{
+    std::vector<int> myVector{ 1, 1, 2, 5, 6, 1, 2, 4 };
+    std::cout << "[IN]: ";
+    for (int i = 0; i < myVector.size(); i++)
+    {
+        std::cout << myVector[i] << " ";
+    }
+    foo(myVector);
+    std::cout << std::endl << "[OUT]: ";
+    for (int i = 0; i < myVector.size(); i++)
+    {
+        std::cout << myVector[i] << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
+
+
+/* сложный вариант с использованием remove_if
+std::vector<int>::iterator result;
+auto iter = myVector.begin();
+for (int i = 0; i < myVector.size(); i++)//мы можем не знать, будет ли уменьшаться размер вектора при 
+    //срабатываении erase в цикле, поэтому оставляем i < myVector size()
+{
+    result = remove_if(next(iter), myVector.end(), [&iter](int& a) {
+        return a == *iter;
+        });
+    myVector.erase(result, myVector.end());
+    iter++;
+}
+sort(myVector.begin(), myVector.end());
+for (int i = 0; i < myVector.size(); i++)
+{
+    std::cout << myVector[i] << " ";
+}*/
---
+++ Lesson_7/ConsoleApplication1_Task_2/ConsoleApplication1_Task_2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <list>
+#include <algorithm>
+
+template<typename T>
+void foo(T& my_case)
+{
+	std::for_each (my_case.begin(), my_case.end(), [](const auto& temp) {
+		std::cout << temp << " ";
+		}); //для ассоциативных упорядоченных контейнеров будет использоваться константный итератор. Для последовательных контейнеров можно обычный
+	std::cout << std::endl;
+}
+
+
+
+int main()
+{
+	std::set<std::string> test_set = { "one","two","three","four" };
+	std::list<std::string> test_list = { "one","two","three","four" };
+	std::vector<std::string> test_vector = { "one","two","three","four" };
+	foo(test_set);
+	foo(test_list);
+	foo(test_vector);
+	return 0;
+}
