aed5344edf809e307a3d071b997bc2e05b6d7d59
Alexander Zhuravlev <a-798@mail.ru>
2023-10-13 09:33:55 +0300

    add Lesson_8

---
+++ Lesson_8/ConsoleApplication1_Task_3/ConsoleApplication1_Task_3.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <memory>
+
+
+template<typename T>
+class my_unique_ptr
+{
+private:
+    T* ptr;
+public:
+
+    my_unique_ptr() //конструктор, принимающий сырой указатель
+    {
+        this->ptr = nullptr;
+    }
+
+    my_unique_ptr(T* ptr)
+    {
+        this->ptr = ptr;
+    }
+
+    T& operator * () //перегружен оператор * для получения объекта
+    {
+        return *ptr;
+    }
+
+    my_unique_ptr(const my_unique_ptr&) = delete;  //запрет конструктора копирования
+    my_unique_ptr& operator = (const my_unique_ptr&) = delete; //запрет оператора присваивания
+
+    my_unique_ptr& release()
+    {
+        delete this->ptr;
+        return *this;
+    }
+
+    ~my_unique_ptr()
+    {
+        delete ptr;
+    }
+};
+
+
+int main()
+{
+    my_unique_ptr<int> my_ptr = new int(5);
+    std::cout << *my_ptr << std::endl;
+    my_ptr.release();
+    *my_ptr = 22;
+    std::cout << *my_ptr;
+    return 0;
+}
