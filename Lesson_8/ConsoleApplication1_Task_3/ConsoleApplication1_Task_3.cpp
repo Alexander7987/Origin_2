732137d565e59f1fbb0a785b76d5852587d7182e
Alexander Zhuravlev <a-798@mail.ru>
2023-10-14 15:26:54 +0300

    change Lesson_8 Task_2

--- Lesson_8/ConsoleApplication1_Task_3/ConsoleApplication1_Task_3.cpp
+++ Lesson_8/ConsoleApplication1_Task_3/ConsoleApplication1_Task_3.cpp
@@ -7,6 +7,7 @@ class my_unique_ptr
 {
 private:
     T* ptr;
+    bool released; //добавляем флаг для безопасного использования release()
 public:
 
     my_unique_ptr() //конструктор, принимающий сырой указатель
@@ -14,9 +15,16 @@ public:
         this->ptr = nullptr;
     }
 
-    my_unique_ptr(T* ptr)
+    my_unique_ptr(T* ptr) : released(false) //как только создали объекть(или переопределили с помощью функтора)
+        //то присваиваем переменной released false, т.к. для этого объекта этот метод еще не был использован
     {
         this->ptr = ptr;
+    }
+
+    my_unique_ptr& operator ()(T* ptr) //функтор для переопределения объекта после использования метода release()
+    {
+        this->ptr = ptr;
+        return *this;
     }
 
     T& operator * () //перегружен оператор * для получения объекта
@@ -27,14 +35,19 @@ public:
     my_unique_ptr(const my_unique_ptr&) = delete;  //запрет конструктора копирования
     my_unique_ptr& operator = (const my_unique_ptr&) = delete; //запрет оператора присваивания
 
-    my_unique_ptr& release()
+    T* release()
     {
-        delete this->ptr;
-        return *this;
+        if (!released)
+        {
+            this->ptr = nullptr;
+            return ptr;
+            released = true;
+        }
     }
 
     ~my_unique_ptr()
     {
+        if (!released)
         delete ptr;
     }
 };
@@ -44,8 +57,10 @@ int main()
 {
     my_unique_ptr<int> my_ptr = new int(5);
     std::cout << *my_ptr << std::endl;
-    my_ptr.release();
     *my_ptr = 22;
-    std::cout << *my_ptr;
+    std::cout << *my_ptr << std:: endl;
+    my_ptr.release();
+    my_ptr (new int(8));
+    std::cout << *my_ptr << std::endl;
     return 0;
 }
